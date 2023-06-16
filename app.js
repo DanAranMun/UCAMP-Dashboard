e1cafa41df2a71c9404102953f77366798e845ef
Dani <danielscout9@gmail.com>
2023-06-15 21:14:06 -0500

    added view and dinamyc file

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.get('/', (req,res)=>{
       res.render('error', { error });
     });   
 })
+app.get('/pokemon', (req,res)=>{
+  res.render('dynamic');  
+})
+app.get('/pokemon/:id', (req,res)=>{
+  const pokemonId = req.params.id;
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    .then(response => {
+      const resources = response.data;
+      res.render('detail', { resources });
+    })
+    .catch(error => {
+      res.render('error', { error });
+    });   
+})
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
