e1468330911c93f722f9f8a31f3721988a9d2c16
Dani <danielscout9@gmail.com>
2023-06-16 12:46:14 -0500

    change config in index.js

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.get('/', (req,res)=>{
-  res.render('dynamic');  
-})
-app.get('/pokemon/:id', (req,res)=>{
-  const pokemonId = req.params.id;
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-    .then(response => {
-      const resources = response.data;
-      res.render('detail', { resources });
-    })
-    .catch(error => {
-      res.render('error', { error });
-    });   
-})
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+module.exports = {
+  '/': (req, res) => {
+    res.render('dynamic');
+  }, '/pokemon/:id': (req, res) => {
+    const pokemonId = req.params.id;
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+      .then(response => {
+        const resources = response.data;
+        res.render('detail', { resources });
+      })
+      .catch(error => {
+        res.render('error', { error });
+      });
+  }
+};
