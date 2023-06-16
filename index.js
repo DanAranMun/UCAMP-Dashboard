7d8c9ef4be8193383ea02efc305cacda95fb7887
Dani <danielscout9@gmail.com>
2023-06-16 12:48:22 -0500

    reversed changes on index.js

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.set('view engine', 'ejs')
+app.set('view engine','ejs')
 
-module.exports = {
-  '/': (req, res) => {
-    res.render('dynamic');
-  }, '/pokemon/:id': (req, res) => {
-    const pokemonId = req.params.id;
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-      .then(response => {
-        const resources = response.data;
-        res.render('detail', { resources });
-      })
-      .catch(error => {
-        res.render('error', { error });
-      });
-  }
-};
+
+app.get('/', (req,res)=>{
+  res.render('dynamic');  
+})
+app.get('/pokemon/:id', (req,res)=>{
+  const pokemonId = req.params.id;
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    .then(response => {
+      const resources = response.data;
+      res.render('detail', { resources });
+    })
+    .catch(error => {
+      res.render('error', { error });
+    });   
+})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
